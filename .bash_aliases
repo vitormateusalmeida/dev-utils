27b444c25577be71f2f0d3530bf0ec8586785424
Vitor Mateus de Almeida <vitormateusalmeida@gmail.com>
2019-08-30 11:31:58 -0300

    Changed bash aliases

--- .bash_aliases
+++ .bash_aliases
@@ -2,3 +2,5 @@ alias update='sudo apt-get update'
 alias upgrade='sudo apt-get update && sudo apt-get upgrade'
 alias dist-upgrade='sudo apt-get update && sudo apt-get dist-upgrade'
 alias docker-stop-all='docker stop $(docker ps -a -q)'
+alias docker-remove-all='docker rm $(docker ps -a -q)'
+
--- add_aliases_in_zsh.sh
+++ add_aliases_in_zsh.sh
@@ -5,12 +5,9 @@ NC='\033[0m'
 
 if [ -f ~/.zshrc ]
   then
+    sudo cp .bash_aliases ~/.bash_aliases
     if ! grep bash_aliases ~/.zshrc > /dev/null
       then
-        if ! [ -f ~/.bash_aliases ]
-          then
-            sudo cp .bash_aliases ~/.bash_aliases
-        fi &&
         echo "if [ -f ~/.bash_aliases ]; then; . ~/.bash_aliases; fi;" >> ~/.zshrc
     fi
   else
